5172682a0175fc763aaa1740bd450daef199e90a
shahria <shahriasayad9@gmail.com>
2024-09-05 14:22:24 +0600

    custom widgets dir added

---
+++ lib/custom_widgets/custom_textformfield.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final IconData icon;
+  final String hintText;
+
+  const CustomTextFormField({
+    Key? key,
+    required this.icon,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          icon,
+          color: Color(0xFF1397D5),
+        ),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          fontSize: 14,
+          color: Color(0xFF1397D5),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(
+            color: Color(0xFF1397D5),
+            width: 2.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          borderSide: BorderSide(
+            color: Color(0xFF1397D5),
+            width: 1.5,
+          ),
+        ),
+        filled: true,
+        fillColor: Color(0xFF1397D5).withOpacity(0.1),
+      ),
+    );
+  }
+}
