2d7e2bad7675471f3558192568e822335e88c928
shahria <shahriasayad9@gmail.com>
2024-09-05 14:18:47 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bk_gas/signup_screens/signup_page1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: signUpPageOne(),
+    );
+  }
+}
