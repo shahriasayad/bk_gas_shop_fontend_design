8b34bbe01d7b18f1783da89d72e3fed5566ce10e
shahria <shahriasayad9@gmail.com>
2024-09-07 02:42:10 +0600

    otp verification input added

--- lib/signup_screens/verify_number.dart
+++ lib/signup_screens/verify_number.dart
@@ -1,8 +1,14 @@
+import 'package:bk_gas/custom_widgets/custom_textformfield_two.dart';
 import 'package:flutter/material.dart';
 
-class numVerifyingPage extends StatelessWidget {
+class numVerifyingPage extends StatefulWidget {
   const numVerifyingPage({super.key});
 
+  @override
+  State<numVerifyingPage> createState() => _numVerifyingPageState();
+}
+
+class _numVerifyingPageState extends State<numVerifyingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +18,55 @@ class numVerifyingPage extends StatelessWidget {
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
         ),
         centerTitle: true,
+      ),
+      body: Material(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: 12),
+              Text(
+                "We have sent a verification code to your number."
+                "Please check your number and enter the code.",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CustomTextFormFieldTwo(),
+                  CustomTextFormFieldTwo(),
+                  CustomTextFormFieldTwo(),
+                  CustomTextFormFieldTwo(),
+                  CustomTextFormFieldTwo(),
+                  CustomTextFormFieldTwo(),
+                ],
+              ),
+              SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceBetween, // Space between left and right text
+                children: [
+                  Text(
+                    "Didn’t get the code?",
+                    style: TextStyle(color: Colors.grey, fontSize: 14),
+                  ),
+                  Text(
+                    "Resend",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF193664),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
