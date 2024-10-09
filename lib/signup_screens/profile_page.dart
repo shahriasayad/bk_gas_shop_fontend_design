b7787b09b3e8bc215a8929d1e19833fe50f36362
shahria <shahriasayad9@gmail.com>
2024-10-09 23:17:55 +0600

    scrollView added

--- lib/signup_screens/profile_page.dart
+++ lib/signup_screens/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:bk_gas/custom_widgets/GenderDropDownMenu.dart';
 import 'package:bk_gas/custom_widgets/custom_button.dart';
 import 'package:bk_gas/custom_widgets/custom_textformfield.dart';
 import 'package:flutter/material.dart';
-
 import '../custom_widgets/custom_address_textfield.dart';
 import '../custom_widgets/date_picker.dart';
 
@@ -35,45 +34,47 @@ class ProfilePage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Material(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Center(
-                child: Container(
-                  height: 163,
-                  width: 148,
-                  // color: Colors.blueAccent,
-                  child: Center(
-                    child: ClipOval(
-                      child: Image.asset(
-                        'assets/images/profile pic.png',
-                        width: 120,
-                        height: 120.0,
-                        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Material(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    height: 163,
+                    width: 148,
+                    // color: Colors.blueAccent,
+                    child: Center(
+                      child: ClipOval(
+                        child: Image.asset(
+                          'assets/images/profile pic.png',
+                          width: 120,
+                          height: 120.0,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Text(
-                "Upload Profile Picture",
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              GenderDropDownMenu(),
-              SizedBox(height: 10),
-              DatePickerField(),
-              SizedBox(height: 10),
-              CustomTextFormField(icon: Icons.email, hintText: "Email"),
-              SizedBox(height: 10),
-              CustomAdressTextfield(hintText: "Address"),
-              SizedBox(height: 30),
-              CustomButton(buttonText: "Continue", onPressed: () {}),
-            ],
+                Text(
+                  "Upload Profile Picture",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GenderDropDownMenu(),
+                SizedBox(height: 10),
+                DatePickerField(),
+                SizedBox(height: 10),
+                CustomTextFormField(icon: Icons.email, hintText: "Email"),
+                SizedBox(height: 10),
+                CustomAdressTextfield(hintText: "Address"),
+                SizedBox(height: 30),
+                CustomButton(buttonText: "Continue", onPressed: () {}),
+              ],
+            ),
           ),
         ),
       ),
